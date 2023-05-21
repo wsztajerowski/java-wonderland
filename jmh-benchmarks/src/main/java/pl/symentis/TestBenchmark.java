d6b17ada6f074d9b139396bacb9385b417db217e
Wiktor-Sztajerowski <92439866+Wiktor-Sztajerowski-sony@users.noreply.github.com>
2023-05-21 18:39:19 +0200

    chore: setup JMH module
    #3

---
+++ jmh-benchmarks/src/main/java/pl/symentis/TestBenchmark.java
@@ -0,0 +1,13 @@
+package pl.symentis;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class TestBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+    }
+
+}
