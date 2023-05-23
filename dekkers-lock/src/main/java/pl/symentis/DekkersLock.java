6515231b84af8f4a2593b8fee645bb41ec15a1ae
Wiktor-Sztajerowski <92439866+Wiktor-Sztajerowski-sony@users.noreply.github.com>
2023-05-23 17:45:44 +0200

    feat: second attempt for dekker's lock
    #13

--- dekkers-lock/src/main/java/pl/symentis/DekkersLock.java
+++ dekkers-lock/src/main/java/pl/symentis/DekkersLock.java
@@ -1,15 +1,40 @@
 package pl.symentis;
 
 public class DekkersLock {
-  private final SharedObject sharedObject;
-  private final DekkerProcess process0;
-  private final DekkerProcess process1;
+  private final Turn turn;
+  final int lockNo;
+  volatile boolean wantToEnter;
+  private DekkersLock otherLock;
 
+  void setOtherLock(DekkersLock otherLock) {
+    this.otherLock = otherLock;
+  }
 
-  public DekkersLock() {
-    sharedObject = new SharedObject();
-    process0 = new DekkerProcess(sharedObject, 0);
-    process1 = new DekkerProcess(sharedObject, 1);
+  static class Turn {
+    public volatile int turn = 0;
+  }
+
+  DekkersLock(Turn turn, int lockNo) {
+    this.turn = turn;
+    this.lockNo = lockNo;
+  }
+
+  public void lock(){
+    wantToEnter = true;
+    while (otherLock.wantToEnter){
+      if (turn.turn != lockNo){
+        wantToEnter = false;
+        while (turn.turn != lockNo){
+          // busy wait
+        }
+        wantToEnter = true;
+      }
+    }
+  }
+
+  public void unlock(){
+    turn.turn = otherLock.lockNo;
+    wantToEnter = false;
   }
 
   public long startTest() throws InterruptedException {
---
+++ dekkers-lock/src/test/java/pl/symentis/SharedObject.java
@@ -0,0 +1,5 @@
+package pl.symentis;
+
+public class SharedObject {
+    public volatile int counter = 0;
+}
