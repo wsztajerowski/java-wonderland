be8724e1d6e07abfc218d9d7f16feb74a9809ec1
Wiktor-Sztajerowski <92439866+Wiktor-Sztajerowski-sony@users.noreply.github.com>
2023-06-13 16:20:11 +0200

    chore: add test for onSpinWait hint
    #15

--- dekkers-lock/src/main/java/pl/symentis/DekkersLockFactory.java
+++ dekkers-lock/src/main/java/pl/symentis/DekkersLockFactory.java
@@ -3,16 +3,20 @@ package pl.symentis;
 public class DekkersLockFactory {
     private final DekkersLock lock0;
     private final DekkersLock lock1;
-    private DekkersLockFactory() {
+    private DekkersLockFactory(boolean useOnSpinWait) {
         DekkersLock.Turn turn = new DekkersLock.Turn();
-        lock0 = new DekkersLock(0, turn);
-        lock1 = new DekkersLock(1, turn);
+        lock0 = new DekkersLock(0, turn, useOnSpinWait);
+        lock1 = new DekkersLock(1, turn, useOnSpinWait);
         lock0.setOtherLock(lock1);
         lock1.setOtherLock(lock0);
     }
 
+    public static DekkersLockFactory get(boolean useOnSpinWait){
+        return new DekkersLockFactory(useOnSpinWait);
+    }
+
     public static DekkersLockFactory get(){
-        return new DekkersLockFactory();
+        return new DekkersLockFactory(false);
     }
 
     public DekkersLock getLock0() {
