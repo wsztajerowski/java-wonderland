82dc06eec69adc25ca33c83cdb29a5114cf3d2f4
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2023-08-18 22:39:21 +0200

    refactor: migrate JMH Morphia entities to records

--- test-wrapper/src/main/java/pl/symentis/entities/jmh/BenchmarkMetadata.java
+++ test-wrapper/src/main/java/pl/symentis/entities/jmh/BenchmarkMetadata.java
@@ -2,17 +2,15 @@ package pl.symentis.entities.jmh;
 
 import dev.morphia.annotations.Entity;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.Map;
 
 @Entity
 public record BenchmarkMetadata(
-    LocalDateTime createdAt,
+    OffsetDateTime createdAt,
     Map<String, String> flamegraphPaths) {
 
     public BenchmarkMetadata(Map<String, String> flamegraphPaths) {
-        this(OffsetDateTime.now(ZoneOffset.UTC).toLocalDateTime(), flamegraphPaths);
+        this(OffsetDateTime.now(), flamegraphPaths);
     }
 }
