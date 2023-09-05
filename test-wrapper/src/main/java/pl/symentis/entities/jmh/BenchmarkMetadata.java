1a6674ce3deaed0547083a34a981a4a92bae1821
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2023-09-05 23:29:27 +0200

    fix: fix converting OffsetDateTime to Local UTC
    #11

--- test-wrapper/src/main/java/pl/symentis/entities/jmh/BenchmarkMetadata.java
+++ test-wrapper/src/main/java/pl/symentis/entities/jmh/BenchmarkMetadata.java
@@ -2,15 +2,17 @@ package pl.symentis.entities.jmh;
 
 import dev.morphia.annotations.Entity;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Map;
 
 @Entity
 public record BenchmarkMetadata(
-    OffsetDateTime createdAt,
+    LocalDateTime createdAt,
     Map<String, String> flamegraphPaths) {
 
     public BenchmarkMetadata(Map<String, String> flamegraphPaths) {
-        this(OffsetDateTime.now(), flamegraphPaths);
+        this(OffsetDateTime.now(ZoneOffset.UTC).toLocalDateTime(), flamegraphPaths);
     }
 }
