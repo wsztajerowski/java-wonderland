f3e3ab139b157e4ab4443b8a796b7d82f101bb04
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-02-05 19:46:02 +0100

    test: add more units for Harris List
    #7

--- harris-linked-list/src/test/java/pl/symentis/HarrisLinkedListTestUtils.java
+++ harris-linked-list/src/test/java/pl/symentis/HarrisLinkedListTestUtils.java
@@ -1,7 +1,10 @@
 package pl.symentis;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.function.Consumer;
 
 public class HarrisLinkedListTestUtils {
     public static <T extends Comparable<T>> String toString(HarrisLinkedList<T> list) {
@@ -13,5 +16,38 @@ public class HarrisLinkedListTestUtils {
         }
         return joiner
             .toString();
+    }
+
+    public static <T extends Comparable<T>> void iterateOverNodes(HarrisLinkedList<T> list, Consumer<Node<T>> consumer) {
+        Node<T> current = list.getHead().getReference();
+        while (current != null) {
+            consumer.accept(current);
+            current = current.getNextNodeMarkableReference().getReference();
+        }
+    }
+
+    public static <T extends Comparable<T>> void iterateOverNodeReferences(HarrisLinkedList<T> list, Consumer<AtomicMarkableReference<Node<T>>> consumer) {
+        AtomicMarkableReference<Node<T>> current = list.getHead();
+        while (current != null) {
+            consumer.accept(current);
+            if(current.getReference() != null) {
+                current = current.getReference().getNextNodeMarkableReference();
+            } else {
+                current =  null;
+            }
+        }
+    }
+
+    public static <T extends Comparable<T>> void markNodesToDelete(HarrisLinkedList<T> list, T ... keys) {
+        List<T> keyList = Arrays.stream(keys).toList();
+        AtomicMarkableReference<Node<T>> current = list.getHead();
+        Node<T> nextNode = current.getReference();
+        while (nextNode != null) {
+            if (keyList.contains(nextNode.getKey())){
+                current.set(nextNode, true);
+            }
+            current = nextNode.getNextNodeMarkableReference();
+            nextNode = current.getReference();
+        }
     }
 }
