42fdf0204929bb6e15cf402f9450811f60559a1c
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-02-12 23:09:16 +0100

    refactor: use proper commit sha as a S3 result object prefix

--- test-wrapper/src/main/java/pl/symentis/commands/ApiCommonSharedOptions.java
+++ test-wrapper/src/main/java/pl/symentis/commands/ApiCommonSharedOptions.java
@@ -8,7 +8,7 @@ import pl.symentis.services.CommonSharedOptions;
 @Command
 public class ApiCommonSharedOptions {
 
-    @Option(names = "--commit-sha", defaultValue = "${GITHUB_SHA}", description = "Commit SHA - you could provide it as a option value or put in GITHUB_SHA env variable")
+    @Option(names = "--commit-sha", defaultValue = "${COMMIT_SHA}", description = "Commit SHA - you could provide it as a option value or put in COMMIT_SHA env variable. Provided value will be truncated to first 8 chars")
     String commitSha;
 
     @Option(names = "--run-attempt", defaultValue = "${GITHUB_RUN_ATTEMPT}", description = "Run attempt no - you could provide it as a option value or put in GITHUB_RUN_ATTEMPT env variable")
@@ -21,6 +21,9 @@ public class ApiCommonSharedOptions {
     String testNameRegex;
 
     public CommonSharedOptions getValues(){
+        if (commitSha != null && commitSha.length() > 8) {
+            commitSha = commitSha.substring(0,8);
+        }
         return new CommonSharedOptions(commitSha, runAttempt, jvmArgs, testNameRegex);
     }
 
