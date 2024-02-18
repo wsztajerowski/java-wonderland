d4c71523ea9ba0bd025525f2e779410079410dc3
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-02-18 22:07:15 +0100

    test: integration test for S3Service

---
+++ test-wrapper/src/test/java/pl/symentis/infra/S3ServiceIT.java
@@ -0,0 +1,45 @@
+package pl.symentis.infra;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.symentis.TestcontainersWithS3BaseIT;
+
+import java.nio.file.Path;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+import static pl.symentis.infra.S3ServiceBuilder.getS3ServiceBuilder;
+
+class S3ServiceIT extends TestcontainersWithS3BaseIT {
+    private S3Service sut;
+
+    @BeforeEach
+    void setupSut(){
+        sut = getS3ServiceBuilder()
+            .withS3Client(awsS3Client)
+            .withBucketName(TEST_BUCKET_NAME)
+            .build();
+    }
+
+    @Test
+    public void test_isLocalstackRunning() {
+        assertThat(LOCAL_STACK_CONTAINER.isRunning())
+            .isTrue();
+    }
+
+    @Test
+    public void test_uploadObjectSuccess() {
+        // given
+        Path sampleFilePath = createPathForTestResource("sample.json");
+
+        // when
+        sut.saveFileOnS3("sample.json", sampleFilePath);
+
+        // then
+        assertThatJson(listObjectsInTestBucket())
+            .isArray()
+            .extracting("Key", "Size")
+            .contains(tuple("sample.json", 59));
+    }
+}
