2ecf8719b41e09a3ab6d803c70448faaeb8636b9
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-02-28 21:31:53 +0100

    chore: capture process output streams and forward it to files

--- test-wrapper/src/main/java/pl/symentis/process/BenchmarkProcessBuilder.java
+++ test-wrapper/src/main/java/pl/symentis/process/BenchmarkProcessBuilder.java
@@ -3,17 +3,38 @@ package pl.symentis.process;
 import pl.symentis.JavaWonderlandException;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BenchmarkProcessBuilder {
     List<String> commands = new ArrayList<>();
+    Path outputPath;
+    Path errorOutputPath;
+
+    private BenchmarkProcessBuilder(){
+        outputPath = Paths.get("output.txt");
+        errorOutputPath = Paths.get("error_output.txt");
+    }
 
     public static BenchmarkProcessBuilder benchmarkProcessBuilder(String benchmarkPath) {
         return new BenchmarkProcessBuilder()
             .addArgument("java")
             .addArgument("-jar")
             .addArgument(benchmarkPath);
+    }
+
+    public BenchmarkProcessBuilder withOutputPath(Path path){
+        this.outputPath = path;
+        return this;
+    }
+
+    public BenchmarkProcessBuilder withErrorOutputPath(Path path){
+        this.errorOutputPath = path;
+        return this;
     }
 
     public BenchmarkProcessBuilder addArgumentWithValue(String name, Object value) {
@@ -44,9 +65,17 @@ public class BenchmarkProcessBuilder {
 
     public Process buildAndStartProcess() {
         ProcessBuilder processBuilder = new ProcessBuilder(commands);
-        processBuilder.inheritIO();
         try {
-            return processBuilder.start();
+            Process process = processBuilder.start();
+            Files.copy(
+                process.getInputStream(),
+                outputPath,
+                StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(
+                process.getErrorStream(),
+                errorOutputPath,
+                StandardCopyOption.REPLACE_EXISTING);
+            return process;
         } catch (IOException e) {
             throw new JavaWonderlandException(e);
         }
