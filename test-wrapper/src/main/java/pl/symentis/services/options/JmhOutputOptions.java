515510422f11f3f09279f3060eb1587bd65403ff
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-04-21 02:18:16 +0200

    refactor: merge jmh output options

--- test-wrapper/src/main/java/pl/symentis/services/options/JmhOutputOptions.java
+++ test-wrapper/src/main/java/pl/symentis/services/options/JmhOutputOptions.java
@@ -3,7 +3,6 @@ import java.nio.file.Path;
 
 public record JmhOutputOptions(
     Path processOutput,
-    Path humanReadableOutput,
     Path machineReadableOutput) {
 
     public static JmhOutputOptionsBuilder jmhOutputOptionsBuilder() {
@@ -12,7 +11,6 @@ public record JmhOutputOptions(
 
     public static final class JmhOutputOptionsBuilder {
         private Path processOutput;
-        private Path humanReadableOutput;
         private Path machineReadableOutput;
 
         private JmhOutputOptionsBuilder() {
@@ -23,18 +21,13 @@ public record JmhOutputOptions(
             return this;
         }
 
-        public JmhOutputOptionsBuilder withHumanReadableOutput(Path humanReadableOutput) {
-            this.humanReadableOutput = humanReadableOutput;
-            return this;
-        }
-
         public JmhOutputOptionsBuilder withMachineReadableOutput(Path machineReadableOutput) {
             this.machineReadableOutput = machineReadableOutput;
             return this;
         }
 
         public JmhOutputOptions build() {
-            return new JmhOutputOptions(processOutput, humanReadableOutput, machineReadableOutput);
+            return new JmhOutputOptions(processOutput, machineReadableOutput);
         }
     }
 }
