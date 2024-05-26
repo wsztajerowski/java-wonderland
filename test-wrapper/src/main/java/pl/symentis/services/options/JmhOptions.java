f412103b49b6f86184428a68c2367ac6d0c6db23
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-05-26 23:37:13 +0200

    chore: remove migrated modules

--- test-wrapper/src/main/java/pl/symentis/services/options/JmhOptions.java
+++
@@ -1,9 +0,0 @@
-package pl.symentis.services.options;
-
-public record JmhOptions(
-    JmhBenchmarkOptions benchmarkOptions,
-    JmhOutputOptions outputOptions,
-    JmhWarmupOptions warmupOptions,
-    JmhIterationOptions iterationOptions,
-    JmhJvmOptions jvmOptions) {
-}
